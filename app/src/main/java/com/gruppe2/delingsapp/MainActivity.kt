7fbe79badc3c993445d8c8f4a810758b5fb86bb2
Andrea <andrea.s.nilsson@hiof.no>
2024-10-07 11:58:23 +0200

    fjernet duplikat av res/view_attributes og feil pakkehenvisnig i MainActivity

--- app/src/main/java/com/gruppe2/delingsapp/MainActivity.kt
+++ app/src/main/java/com/gruppe2/delingsapp/MainActivity.kt
@@ -5,8 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.delingsapp.ui.navigation.AppNavHost
-import com.example.delingsapp.viewmodel.UserViewModel
+import com.gruppe2.delingsapp.ui.navigation.AppNavHost
+import com.gruppe2.delingsapp.viewmodel.UserViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
