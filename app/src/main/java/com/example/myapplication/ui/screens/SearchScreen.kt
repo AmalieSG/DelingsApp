36c8f4e0f7582a80e04f30608b6af1ebeee97058
Amalie <iagresho@hiof.no>
2024-10-17 20:01:15 +0200

    Update SearchScreen.kt

--- app/src/main/java/com/example/myapplication/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/SearchScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.R
 import com.example.myapplication.components.SearchBar
 import com.example.myapplication.components.ProductList
